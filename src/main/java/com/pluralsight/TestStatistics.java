c00961bc4a7cad573cd94eebda15900edbdc0541
sgrinley <shamar808@yahoo.com>
2026-04-16 17:17:23 -0700

    Initial commit

---
+++ src/main/java/com/pluralsight/TestStatistics.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+public class TestStatistics {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
+
+
+
+// Step 1: Create an array of 10 test score
+// Step 2: Variables for calculations
+// Step 3: Loop through array
+// Step 4: Calculate average
+// Step 5: Calculate median
+// Step 6: Display results
